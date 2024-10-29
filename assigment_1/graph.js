4c368b4aa0abbba5c26894c6b2442f6072dc1dcd
riccardocicala <riccardo.cicala11@gmail.com>
2024-10-29 10:15:08 +0100

    -ASSIGNMENT 1 reorder files

--- assigment_1/graph.js
+++ assigment_1/graph.js
@@ -1,4 +1,4 @@
-d3.csv("../assignment_1/co-emissions-per-capita-last-decade.csv", function(d) { // non funziona
+d3.csv("co-emissions-per-capita-last-decade.csv", function(d) { // non funziona
 	return {
 		entity: d.Entity,
 		code: d.Code,
