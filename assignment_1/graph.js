67f8240ee75d458d5ac99039f4b7c3fbe0a1a046
riccardocicala <riccardo.cicala11@gmail.com>
2024-10-31 10:58:48 +0100

    first plot without hover

--- assignment_1/graph.js
+++ assignment_1/graph.js
@@ -1,10 +1,55 @@
-d3.csv("co-emissions-per-capita-last-decade.csv", function(d) { // non funziona
+const margin = {top: 30, right: 30, bottom: 70, left: 60},
+width = 460 - margin.left - margin.right,
+height = 400 - margin.top - margin.bottom;
+
+const svg = d3.select("#plot1")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", `translate(${margin.left},${margin.top})`);
+
+d3.csv("co-emissions-per-capita-last-year.csv", function(d) { // non funziona
 	return {
 		entity: d.Entity,
 		code: d.Code,
-		emissions: +d['Annual CO₂ emissions (per capita)'],
+		emissions: +d['Annual CO2 emissions (per capita)'],
 	  };
 })
 .then(function (data) {
-    console.log(data);
-})
+	console.log(data);
+	var max_value = 0;
+	data.forEach(d => {
+		if(d.emissions>max_value){
+			max_value = d.emissions;
+		}
+	});
+	const x = d3.scaleBand()
+	.range([ 0, width ])
+	.domain(data.map(d => d.code))
+	.padding(0.2);
+	svg.append("g")
+	.attr("transform", `translate(0, ${height})`)
+	.call(d3.axisBottom(x))
+	.selectAll("text")
+	.attr("transform", "translate(-10,0)rotate(-45)")
+	.style("text-anchor", "end");
+
+	// Add Y axis
+	const y = d3.scaleLinear()
+	.domain([0, max_value])
+	.range([ height, 0]);
+	svg.append("g")
+	.call(d3.axisLeft(y));
+  
+	// Bars
+	svg.selectAll("mybar")
+	.data(data)
+	.join("rect")
+	.attr("x", d => x(d.code))
+	.attr("y", d => y(d.emissions))
+	.attr("width", x.bandwidth())
+	.attr("height", d => height - y(d.emissions))
+	.attr("fill", "#69b3a2");
+  
+});
--- assignment_1/pages_auxiliaries.js
+++ assignment_1/pages_auxiliaries.js
@@ -3,14 +3,12 @@ document.write(`
     <meta charset="UTF-8">
     <meta name="theme-color" content="#2e68c0">
     <link rel="stylesheet" type="text/css" href="${location.pathname.includes('assignment_1') ? '../main.css' : 'main.css'}"/>
-    <script src="https://d3js.org/d3.v7.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
-    <script src="${location.pathname.includes('assignment_1') ? 'graph.js' : 'pages/graph.js'}"></script>
     <title>Chart Troopers' Website</title>
 `);
 
