fb1639214f3987b452cf931f9b9947b7db361101
riccardocicala <riccardo.cicala11@gmail.com>
2024-10-31 09:34:07 +0100

    Undo the last commit (in not a smart way)

--- assignment_1/pages_auxiliaries.js
+++ assignment_1/pages_auxiliaries.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const navbar = document.createElement("nav");
     navbar.id = "navbar";
     navbar.innerHTML = `<h1 id="title" style="cursor: pointer;">
-                           <a href="https://andrestc21.github.io/Data_Visualization_Chart_Troopers/" style="text-decoration: none">
+                           <a href="https://andrestc21.github.io/Data_Visualization_Chart_Troopers/" style="text-decoration: none; color: inherit;">
                                Data Visualization Website
                            </a>
                        </h1>`;
