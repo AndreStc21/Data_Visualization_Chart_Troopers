c622881f3254619644f8c2714f8f797046620acc
riccardocicala <riccardo.cicala11@gmail.com>
2024-11-04 23:33:29 +0100

    tooltip+normalized stacked

--- assignment_1/graph.js
+++ assignment_1/graph.js
@@ -23,6 +23,13 @@ const svg_plot3 = d3.select("#plot3")
 .append("g")
 .attr("transform", `translate(${margin.left},${margin.top})`);
 
+const svg_plot5 = d3.select("#plot5")
+.append("svg")
+.attr("width", width + margin.left + margin.right)
+.attr("height", height + margin.top + margin.bottom)
+.append("g")
+.attr("transform", `translate(${margin.left},${margin.top})`);
+
 const svg_plot6 = d3.select("#plot6")
 .append("svg")
 .attr("width", width + margin.left + margin.right)
@@ -73,7 +80,7 @@ function bar_plot(data, svg_plot, id_div){
 	var mouseover = function(event, d) {
 		d3.selectAll(id_div+"  rect").style("opacity", 0.2);
 		d3.select(this).style("opacity", 1);
-		info = d3.select(this).datum()
+		info = d3.select(this).datum();
 		tooltip.html("Country: " + info.entity + "<br>" + "CO2 emissions: " + info.emissions).style("opacity", 1);
 	}
 
@@ -99,20 +106,30 @@ function bar_plot(data, svg_plot, id_div){
     .on("mouseleave", mouseleave);
 }
 
-function stacked_bar_plot(data, svg_plot, id_div){
-	// Get data for the stacked bar plot
+function stacked_bar_plot(data, svg_plot, id_div, normalized){
 	const nestedData = Array.from(
-		d3.group(data, d => d.region),  // Group data by 'region'
+		d3.group(data, d => d.region),
 		([region, values]) => {
-		  const regionData = { region: region };
+		  const regionData = { region: region};
 		  values.forEach(v => regionData[v.rank] = v.emissions);
 		  return regionData;
 		}
-	  );
+	);
+
+	var mapping_entity = {};
+	data.forEach(d => mapping_entity[d.region+d.rank] = d.entity);
 	  
 
 	const subgroups = Array.from(new Set(data.map(d => d.rank)));
 	const groups = Array.from(new Set(data.map(d => d.region)));
+
+	if(normalized){
+		for(let i=0; i<6; i++){
+			let value_region = 0;
+			subgroups.forEach(j => value_region += nestedData[i][j])
+			subgroups.forEach(j => nestedData[i][j] = nestedData[i][j]/value_region*100)
+		}
+	}
 
 	// Get the max value for the Y axis
 	var max_value = 0;
@@ -138,31 +155,59 @@ function stacked_bar_plot(data, svg_plot, id_div){
 	.style("text-anchor", "end");
 
 	// Add Y axis
-	const y = d3.scaleLinear()
-	.domain([0, max_value+(0.1*max_value)])
-	.range([ height, 0]);
+	var y;
+	if(normalized){
+		y = d3.scaleLinear()
+		.domain([0, max_value])
+		.range([ height, 0]);
+	}
+	else{
+		y = d3.scaleLinear()
+		.domain([0, max_value+(0.1*max_value)])
+		.range([ height, 0]);
+	}
 
 	svg_plot.append("g")
 	.call(d3.axisLeft(y));
 
-	// color palette = one color per every subgroup
+	var tooltip = d3.select("body")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "1px")
+    .style("border-radius", "5px")
+    .style("padding", "10px")
+	.style("position", "absolute")
+	.style("left", "0px")
+	.style("top", "0px");
+
+	var mouseover = function(event, d) {
+		d3.selectAll(id_div+"  rect").style("opacity", 0.2);
+		d3.select(this).style("opacity", 1);
+		info = d3.select(this).datum();
+		info_parent = d3.select(this.parentNode).datum();
+		tooltip.html("Country: " + mapping_entity[info.data.region+info_parent.key] + "<br>" + "Value: " + info.data[info_parent.key]).style("opacity", 1);
+	}
+
+	var mousemove = function(event, d) {
+		tooltip.style('left', (event.pageX+20) + 'px').style('top', (event.pageY) + 'px');
+	}
+
+	var mouseleave = function(event, d) {
+		d3.selectAll(id_div+" rect").style("opacity",0.8);
+		tooltip.style("opacity", 0);
+	}
+
 	const color = d3.scaleOrdinal()
 	.domain(subgroups)
 	.range(['#5778a4', '#e49444', '#d1615d','#85b6b2','#6a9f58', '#e7ca60']);
 
-	// stacko male i dati, guardate come funziona .stack() per capire meglio. La parte critica comunque è quella qua sotto
-
-	//stack the data per subgroup
 	const stackedData = d3.stack()
     .keys(subgroups)
-    (nestedData); //usando data non funziona comunque, penso perchè la struttura che abbiamo noi non ha sottogruppi con lo stesso nome. O provate ad usare la colonna Rank, ma non so poi nel dettaglio come fargli vedere i dati correlato alla riga giusta
+    (nestedData);
 
-	/* const stackedData = d3.stack()
-    .keys(d3.union(data.map(d => d.entity)))
-    .value(([, group], key) => group.get(key).emissions)
-    (d3.index(data, d => d.region, d => d.entity)); */
-
-	// Show the bars
 	svg_plot.append("g")
 	.selectAll("g")
 	// Enter in the stack data = loop key per key = group per group
@@ -178,6 +223,9 @@ function stacked_bar_plot(data, svg_plot, id_div){
 	.attr("height", d => y(d[0]) - y(d[1]))
 	.attr("width",x.bandwidth())
 	.attr("stroke", "grey")
+	.on("mouseover", mouseover)
+	.on("mousemove", mousemove)
+    .on("mouseleave", mouseleave);
 }
 
 function heatmap_plot(data, svg_plot, id_div){
@@ -232,7 +280,7 @@ function heatmap_plot(data, svg_plot, id_div){
 	var mouseover = function(event, d) {
 		d3.selectAll(id_div+"  rect").style("opacity", 0.2);
 		d3.select(this).style("opacity", 1);
-		info = d3.select(this).datum()
+		info = d3.select(this).datum();
 		tooltip.html("Country: " + info.entity + "<br>" + "Value: " + info.value).style("opacity", 1);
 	}
 
@@ -290,7 +338,8 @@ d3.csv("co-emissions-per-capita-last-year-region-comparison.csv", function(d) {
 	  };
 })
 .then(function (data) {
-	stacked_bar_plot(data, svg_plot3, "#plot3");
+	stacked_bar_plot(data, svg_plot3, "#plot3", false);
+	stacked_bar_plot(data, svg_plot5, "#plot5", true);
 });
 
 
