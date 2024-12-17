6a3b6050434a85ea33bbfa5e281b47f7891bff71
riccardocicala <riccardo.cicala11@gmail.com>
2024-12-17 23:20:06 +0100

    not so much

--- assignment_4/graph.js
+++ assignment_4/graph.js
@@ -1,3 +1,39 @@
+var checkList = document.getElementById('checkbox1');
+var clicked_years = new Array()
+checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
+  if (checkList.classList.contains('visible'))
+    checkList.classList.remove('visible');
+  else
+    checkList.classList.add('visible');
+}
+
+const checkbox = document.querySelectorAll("input[type='checkbox']");
+
+for(check of checkbox){
+	check.addEventListener('change', (event) => {
+		if (event.currentTarget.checked) {
+			clicked_years.push(event.currentTarget.value);
+			let plot = document.getElementById("svg1");
+			while (plot.firstChild) {
+				plot.removeChild(plot.lastChild);
+			}
+			line_plot(avg_data, svg_plot1, "#plot1", clicked_years);
+		} else {
+			var index = clicked_years.indexOf(event.currentTarget.value);
+			if (index > -1) {
+				clicked_years.splice(index, 1);
+			}
+			let plot = document.getElementById("svg1");
+			while (plot.firstChild) {
+				plot.removeChild(plot.lastChild);
+			}
+			line_plot(avg_data, svg_plot1, "#plot1", clicked_years);
+		}
+	})
+}
+clicked_years.push('2024')
+
+
 const margin = { top: 30, right: 30, bottom: 100, left: 100 },
 	width = 550 - margin.left - margin.right,
 	height = 550 - margin.top - margin.bottom;
@@ -8,7 +44,9 @@ const svg_plot1 = d3
 	.attr("width", width + margin.left + margin.right)
 	.attr("height", height + margin.top + margin.bottom)
 	.append("g")
-	.attr("transform", `translate(${margin.left},${margin.top})`);
+	.attr("transform", `translate(${margin.left},${margin.top})`)
+	.attr("id", "svg1");
+
 
 
 function add_axis_label(svg_plot, x, y, transform, text_anchor, label) {
@@ -21,11 +59,12 @@ function add_axis_label(svg_plot, x, y, transform, text_anchor, label) {
 		.text(label);
 }
 
-function line_plot(data, svg_plot, plot_id) {
+function line_plot(data, svg_plot, plot_id, years) {
+	data = data.filter(d => years.includes(d.year))
 	
-	// Add X axis --> it is a date format
-    var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.month; }))
+	// Add X axis
+    var x = d3.scaleLinear()
+      .domain(['1', '12'])
       .range([ 0, width ]);
 	  svg_plot.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -47,8 +86,10 @@ function line_plot(data, svg_plot, plot_id) {
       .attr("d", d3.line()
         .x(function(d) { return x(d.month) })
         .y(function(d) { return y(d.temperature) })
-        )
+		)
 	}
+
+avg_data = new Array()
 
 d3.csv("monthly_avg_per_year.csv", function (d) {
 	return {
@@ -57,5 +98,6 @@ d3.csv("monthly_avg_per_year.csv", function (d) {
 		month: d.month,
 	};
 }).then(function (data) {
-	line_plot(data, svg_plot1, "#plot1");
+	avg_data = data;
+	line_plot(avg_data, svg_plot1, "#plot1", clicked_years);
 });
